938e95d16e79465100c8a165a96d0de3d39ad427
HuynPhuc <phuc.huynhphuc2k2@hcmut.edu.vn>
2022-11-06 02:16:37 +0700

    first commit

---
+++ project/Core/Inc/led_display.h
@@ -0,0 +1,16 @@
+/*
+ * led_display.h
+ *
+ *  Created on: Nov 5, 2022
+ *      Author: Admin
+ */
+
+#ifndef INC_LED_DISPLAY_H_
+#define INC_LED_DISPLAY_H_
+
+
+void display7SEG(int num);
+void count_down();
+void blink_led();
+
+#endif /* INC_LED_DISPLAY_H_ */
---
+++ project/Core/Src/global.c
@@ -0,0 +1,9 @@
+/*
+ * global.c
+ *
+ *  Created on: Nov 4, 2022
+ *      Author: Admin
+ */
+#include "global.h"
+
+int counter = 9;
